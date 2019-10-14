3a24cc50d7506da9276cba787109474d5a10fd8a
Christoffer Book <Alis4311@gmail.com>
2019-10-14 16:05:17 +0200

    Camera

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ListItem from "./components/ListItem"
+import ListItem from "./components/ListItem" 
 
 const products=[{
 	_id:1,
