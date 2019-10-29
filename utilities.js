c30ef27c22a1b969313bdafeaa8b4a58af1b179c
Oscar Andersson <oscar.andersson3@ikea.com>
2019-10-29 09:37:34 +0100

    add search utility

--- utilities.js
+++ utilities.js
@@ -4,14 +4,27 @@ export async function GetProduct(id) {
 
         let response = await (await fetch("https://europe-west2-ikea-mau-eu.cloudfunctions.net/api/getProduct/" + id)).json();
 
-        console.log('response: ', response);
+        if(response){
+            console.log('response: ', response);
 
-        let product = response.product;
-
-        console.log('product: ', product);
-
-        if(!product.error){
-            return product;
+            let product = response.product;
+    
+            console.log('product: ', product);
+    
+            if(!product.error){
+                return product;
+            }
         }
     }
+}
+
+export async function GetSearchResult(query){
+        console.log('Searching for ', query);
+        let response = await (await fetch("https://europe-west2-ikea-mau-eu.cloudfunctions.net/api/search/" + query)).json();
+        console.log('Search complete, found ' + response.length + ' results');
+    
+        if(response.length){
+            console.log(`Search successful, found ${response.length} matches.`)
+            return response;
+        }
 }
